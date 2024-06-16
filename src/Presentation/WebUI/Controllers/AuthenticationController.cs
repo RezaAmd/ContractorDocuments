6ab4d108807172718d79dca9fed4d5addfa62e78
Reza Ahmadi <Rezaahmadidvlp@gmail.com>
2024-06-16 09:41:27 +0330

    🌱 Initialize project layout.

---
+++ src/Presentation/WebUI/Controllers/AuthenticationController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        #region Fields
+
+
+
+        #endregion
+
+        #region Ctor
+
+
+
+        #endregion
+
+        #region Pages
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        #endregion
+    }
+}
