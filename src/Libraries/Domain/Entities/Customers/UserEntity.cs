98c40a4ac48f1a9f4a02638575d3dc9bf9ae4f04
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2024-06-25 18:32:20 +0330

    ♻️ Resolve - Entity framework warning on "Fullname" & "PasswordHash" nullable.

--- src/Libraries/Domain/Entities/Customers/UserEntity.cs
+++ src/Libraries/Domain/Entities/Customers/UserEntity.cs
@@ -4,9 +4,9 @@ namespace ContractorDocuments.Domain.Entities.Customers
 {
     public class UserEntity : BaseEntity
     {
-        public Fullname? Fullname { get; set; }
+        public Fullname Fullname { get; set; }
         public required string PhoneNumber { get; set; }
-        public PasswordHash? Password { get; set; } = null;
+        public PasswordHash Password { get; set; }
         public DateTime CreatedOn { get; private set; } = DateTime.Now;
 
         #region Relations
