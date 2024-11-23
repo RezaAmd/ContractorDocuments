f091733cdd6ba20b16bdaf0094978187618ba5f0
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2024-11-23 20:04:04 +0330

    🌱 Initialize web API presentation.

---
+++ src/Presentation/WebApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> SignIn()
+        {
+            return Ok();
+        }
+    }
+}
