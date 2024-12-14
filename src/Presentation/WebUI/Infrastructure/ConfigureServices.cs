a27bfef1269aa144d314613cd27908b5120ecdc4
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2024-12-15 00:51:14 +0330

    ♻️ Refactoring

--- src/Presentation/WebUI/Infrastructure/ConfigureServices.cs
+++ src/Presentation/WebUI/Infrastructure/ConfigureServices.cs
@@ -1,20 +1,10 @@
-﻿using ContractorDocuments.WebUI.Areas.Admin.Factories;
-
-namespace ContractorDocuments.WebUI.Infrastructure
+﻿namespace ContractorDocuments.WebUI.Infrastructure
 {
     public static class ConfigureServices
     {
         public static IServiceCollection AddDependencyServices(this IServiceCollection services)
         {
             services.AddApplicationServices();
-
-            #region Admin
-
-            services
-                .AddScoped<UserModelFactory>()
-                ;
-
-            #endregion
 
             return services;
         }
