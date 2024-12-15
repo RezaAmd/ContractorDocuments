d1c2f4bcd4c3b04f6b55659a2b16dad10b85cfe3
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2024-12-16 01:07:22 +0330

    🌱 Inject mediator.

--- src/Presentation/WebApi/Controllers/AuthenticationController.cs
+++ src/Presentation/WebApi/Controllers/AuthenticationController.cs
@@ -1,11 +1,23 @@
-﻿namespace WebApi.Controllers
+﻿using MediatR;
+
+namespace WebApi.Controllers
 {
     [ApiController]
     [Route("[controller]/[action]")]
     public class AuthenticationController : Controller
     {
+        #region DI & Ctor
+
+        private readonly IMediator _mediator;
+        public AuthenticationController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        #endregion
+
         [HttpPost]
-        public async Task<IActionResult> SignInPassword()
+        public async Task<IActionResult> SignInPassword(CancellationToken cancellationToken)
         {
             return Ok();
         }
