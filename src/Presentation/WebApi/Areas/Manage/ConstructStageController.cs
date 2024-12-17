4e4204c1f7704b50b1b074519e90794521640e54
Reza Ahmadi <r.ahmadi@rima.tax>
2024-12-17 16:55:59 +0330

    🌱 Implement endpoint of construct stage controller.

---
+++ src/Presentation/WebApi/Areas/Manage/ConstructStageController.cs
@@ -0,0 +1,56 @@
+﻿using ContractorDocuments.Application.ConstructStages.Commands;
+using ContractorDocuments.Application.ConstructStages.Queries;
+using ContractorDocuments.Domain.Enums;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ContractorDocuments.WebApi.Areas.Manage
+{
+    [ApiController]
+    [Area("Manage")]
+    [Route("[area]/[controller]/[action]")]
+    [Authorize]
+    public class ConstructStageController : ControllerBase
+    {
+        #region DI & Ctor
+
+        private readonly IMediator _mediator;
+        public ConstructStageController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        #endregion
+
+        #region Commands
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateConstructStageCommand createCommand,
+            CancellationToken cancellationToken)
+        {
+            var createResult = await _mediator.Send(createCommand, cancellationToken);
+            if (createResult.IsSuccess == false)
+                return BadRequest(createResult);
+
+            return Ok(createResult);
+        }
+
+        #endregion
+
+        #region Queries
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll(ProjectType? projectTypeId,
+            CancellationToken cancellationToken)
+        {
+            var getAllStagesQuery = new GetAllConstructStagesQuery();
+            if (projectTypeId != null)
+                getAllStagesQuery.ProjectTypeId = projectTypeId;
+
+            var constructStages = await _mediator.Send(getAllStagesQuery, cancellationToken);
+
+            return Ok(constructStages);
+        }
+
+        #endregion
+    }
+}
