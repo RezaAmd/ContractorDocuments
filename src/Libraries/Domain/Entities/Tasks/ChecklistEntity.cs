d7bcf29925e28603cc3672b5511dc4477508885d
Reza Ahmadi <r.ahmadi@rima.tax>
2024-12-24 17:22:22 +0330

    🌱 Add task entity.

---
+++ src/Libraries/Domain/Entities/Tasks/ChecklistEntity.cs
@@ -0,0 +1,18 @@
+﻿using ContractorDocuments.Domain.Entities.Customers;
+
+namespace ContractorDocuments.Domain.Entities.Tasks
+{
+    public class ChecklistEntity : BaseEntity, IDateTimeCreation
+    {
+        public required string Title { get; set; }
+        public DateTime CreatedOn { get; private set; } = DateTime.UtcNow;
+
+        #region Relations
+        public Guid OwnerId { get; set; }
+        public virtual UserEntity? User { get; set; }
+
+        public Guid TaskId { get; set; }
+        public virtual TaskEntity? Task { get; set; }
+        #endregion
+    }
+}
---
+++ src/Libraries/Domain/Entities/Tasks/TaskEntity.cs
@@ -0,0 +1,40 @@
+﻿using ContractorDocuments.Domain.Entities.Customers;
+using ContractorDocuments.Domain.Entities.Projects;
+
+namespace ContractorDocuments.Domain.Entities.Tasks
+{
+    public class TaskEntity : BaseEntity, IDateTimeCreation
+    {
+        public required string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedOn { get; private set; } = DateTime.UtcNow;
+
+        #region Relations
+        public Guid OwnerId { get; set; }
+        public virtual UserEntity? User { get; set; }
+
+        public Guid ProjectId { get; set; }
+        public virtual ProjectEntity? Project { get; set; }
+
+        public Guid? ProjectStageId { get; set; }
+        public virtual ProjectStageEntity? ProjectStage { get; set; }
+
+        public virtual ICollection<ChecklistEntity>? CheckLists { get; set; }
+        #endregion
+
+        #region Ctor
+        TaskEntity() { }
+        public TaskEntity(string title, Guid ownerId, Guid projectId, Guid? projectStageId)
+        {
+            Title = title;
+            OwnerId = ownerId;
+            ProjectId = projectId;
+
+            if (projectStageId != null || projectStageId != Guid.Empty)
+            {
+                ProjectStageId = projectStageId;
+            }
+        }
+        #endregion
+    }
+}
